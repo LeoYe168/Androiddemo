e6cd98ecca5409f9e0691ece0841f711d0d8f681
yeyao <164307696@qq.com>
2016-01-12 14:48:27 +0800

    support material design 包中控件学习

---
+++ app/src/main/java/com/leoye/androiddemo/MaterialDesignActivity.java
@@ -0,0 +1,40 @@
+package com.leoye.androiddemo;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MaterialDesignActivity extends FragmentActivity implements View.OnClickListener {
+
+
+    TextView snackBarText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_design);
+
+        initView();
+
+    }
+
+    private void initView() {
+        snackBarText = (TextView) findViewById(R.id.text_snackbar);
+        snackBarText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.text_snackbar:
+                Snackbar.make(v, "your message", Snackbar.LENGTH_INDEFINITE).setAction("click button", this).show();
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+
